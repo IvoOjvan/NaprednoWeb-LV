2f97f08fa893155d05cb352bbd56e17a23133a5f
iojvan <ivos.ojvan@gmail.com>
2025-03-10 16:59:43 +0100

    Fix layout with bootstrap classes.

--- LV1/index.php
+++ LV1/index.php
@@ -11,27 +11,34 @@
 </head>
 
 <body>
-    <form action="index.php" method="POST" class="col-4 m-4">
-        <h1>Odaberi broj stranice</h1>
-        <div class="row mb-3">
-            <div class="col-6">
-                <select name="page_number" class="form-select" aria-label="Default select example" id="page-num-select">
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                </select>
-            </div>
-            <div class="col-6">
-                <button type="submit" class="btn btn-primary">Odaberi</button>
-            </div>
+    <div class="container-fluid">
+        <div class="row">
+            <form action="index.php" method="POST" class="col-4 my-4 mx-4">
+                <h1>Odaberi broj stranice</h1>
+                <div class="row mb-3">
+                    <div class="col-6">
+                        <select name="page_number" class="form-select" aria-label="Default select example"
+                            id="page-num-select">
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <button type="submit" class="btn btn-primary">Odaberi</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
 
-    <div class="row">
-        <?php include('curl.php'); ?>
+
+        <div class="row">
+            <?php include('curl.php'); ?>
+        </div>
     </div>
+
 </body>
 
 </html>
